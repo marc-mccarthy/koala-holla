ec13a888aab672c6fd72cc7a9ec846048fa819c4
theoryofmars <marstheory20@gmail.com>
2022-05-18 17:13:46 -0500

    Changed delete request to use url with id.

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,87 +4,88 @@ const pg = require('pg');
 
 // DB CONNECTION
 const pool = new pg.Pool({
-    database: 'koala-holla',
-    host: 'localhost',
-    port: 5432,
-    max: 12,
-    idleTimeoutMillis: 30000 
+   database: 'koala-holla',
+   host: 'localhost',
+   port: 5432,
+   max: 12,
+   idleTimeoutMillis: 30000 
 });
 
 // GET
 koalaRouter.get('/', (req, res) => {
-    console.log('GET /inventory');
-    let queryString = 'SELECT * FROM inventory;';
-    pool.query(queryString)
-    .then(result => {
-        res.send(result.rows);
-    }).catch(error => {
-        console.log(error);
-        res.sendStatus(500);
-    })
+   console.log('GET /inventory');
+   let queryString = 'SELECT * FROM inventory;';
+   pool.query(queryString)
+   .then(result => {
+      res.send(result.rows);
+   }).catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+   })
 })
 
 // POST
 koalaRouter.post('/', (req, res) => {
-    console.log('POST /inventory');
-    const queryString = 'INSERT INTO inventory (name, gender, age, ready_to_transfer, notes) VALUES ($1, $2, $3, $4, $5);';
-    let values = [req.body.name, req.body.gender, req.body.age, req.body.readyForTransfer, req.body.notes];
-    pool.query(queryString, values)
-    .then(result => {
-        res.sendStatus(201);
-    }).catch(error => {
-        console.log(error);
-        res.sendStatus(500);
-    })
+   console.log('POST /inventory');
+   const queryString = 'INSERT INTO inventory (name, gender, age, ready_to_transfer, notes) VALUES ($1, $2, $3, $4, $5);';
+   let values = [req.body.name, req.body.gender, req.body.age, req.body.readyForTransfer, req.body.notes];
+   pool.query(queryString, values)
+   .then(result => {
+      res.sendStatus(201);
+   }).catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+   })
 }) 
 
 // PUT
 koalaRouter.put('/', (req, res) => {
-    console.log('PUT /inventory');
-    returnReadyForTransfer(req.body.id)
-    .then(result => {
-        if (result === 'N') {
-            const queryString = `UPDATE inventory SET ready_to_transfer = 'Y' WHERE id = ${req.body.id};`;
-            pool.query(queryString)
-            .then(result => {
-                res.sendStatus(201);
-            }).catch(error => {
-                console.log(error);
-                res.sendStatus(500);
-            })
-        } else {
-            const queryString = `UPDATE inventory SET ready_to_transfer = 'N' WHERE id = ${req.body.id};`;
-            pool.query(queryString)
-            .then(result => {
-                res.sendStatus(201);
-            }).catch(error => {
-                console.log(error);
-                res.sendStatus(500);
-            })
-        }
-    })
+   console.log('PUT /inventory');
+   returnReadyForTransfer(req.body.id)
+   .then(result => {
+      if (result === 'N') {
+         const queryString = `UPDATE inventory SET ready_to_transfer = 'Y' WHERE id = ${req.body.id};`;
+         pool.query(queryString)
+         .then(result => {
+            res.sendStatus(201);
+         }).catch(error => {
+            console.log(error);
+            res.sendStatus(500);
+         })
+      } else {
+         const queryString = `UPDATE inventory SET ready_to_transfer = 'N' WHERE id = ${req.body.id};`;
+         pool.query(queryString)
+         .then(result => {
+            res.sendStatus(201);
+         }).catch(error => {
+            console.log(error);
+            res.sendStatus(500);
+         })
+      }
+   })
 })
 
 // DELETE
 koalaRouter.delete('/', (req, res) => {
-    console.log('DELETE /inventory');
-    const queryString = `DELETE FROM inventory WHERE id = ${req.body.id};`;
-    pool.query(queryString)
-    .then(result => {
-        res.sendStatus(201);
-    }).catch(error => {
-        console.log(error);
-        res.sendStatus(500);
-    })
+   console.log('DELETE /inventory');
+   const queryString = `DELETE FROM inventory WHERE id = $1`;
+   values = [req.query.id]
+   pool.query(queryString, values)
+   .then(result => {
+      res.sendStatus(201);
+   }).catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+   })
 })
 
 function returnReadyForTransfer(id) {
-    const queryString = `SELECT ready_to_transfer FROM inventory WHERE id = ${id};`;
-    return pool.query(queryString)
-    .then(result => {
-        let letter = result.rows[0].ready_to_transfer;
-        return letter;
-    })
+   const queryString = `SELECT ready_to_transfer FROM inventory WHERE id = ${id};`;
+   return pool.query(queryString)
+   .then(result => {
+      let letter = result.rows[0].ready_to_transfer;
+      return letter;
+   })
 }
 
 module.exports = koalaRouter;
